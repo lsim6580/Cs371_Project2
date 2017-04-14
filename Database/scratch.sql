0533dc90dfb5e6492fd44107c19ba9abe30ff91d
luke simmons <luke simmons>
2017-04-13 23:21:14 -0500

    added initial database code

---
+++ Database/scratch.sql
@@ -0,0 +1,48 @@
+DROP TABLE IF EXISTS Moderators;
+DROP TABLE IF EXISTS Advertisements;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Categories;
+DROP TABLE IF EXISTS Statuses;
+
+Create TABLE IF NOT EXISTS Users
+(
+  userID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  userFirstName VARCHAR(20) NOT NULL,
+  userLastName VARCHAR(20) NOT NULL,
+  CONSTRAINT pk_users PRIMARY KEY (userID)
+);
+
+CREATE TABLE IF NOT EXISTS Moderators(
+  UserID SMALLINT UNSIGNED NOT NULL,
+  CONSTRAINT pk_moderators PRIMARY KEY (UserID),
+  CONSTRAINT fk_moderator_userID FOREIGN KEY (UserID) REFERENCES Users(UserID) ON DELETE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS Statuses(
+  statusID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  statusName VARCHAR(20) NOT NULL,
+  CONSTRAINT pk_statuses PRIMARY KEY (statusID)
+);
+
+CREATE TABLE IF NOT EXISTS Categories(
+  categoryID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  categoryName VARCHAR(20),
+  CONSTRAINT pk_categories PRIMARY KEY (categoryID)
+);
+
+CREATE TABLE IF NOT EXISTS Advertisements(
+  advertisementID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  advertisementTitle VARCHAR(20) NOT NULL,
+  advertisementDetails TEXT NOT NULL ,
+  advertisementDateTime DATETIME NOT NULL,
+  price FLOAT(10,2) NOT NULL,
+  userID SMALLINT UNSIGNED NOT NULL,
+  moderatorID SMALLINT UNSIGNED NOT NULL,
+  categoryID SMALLINT UNSIGNED NOT NULL,
+  statusID SMALLINT UNSIGNED NOT NULL,
+  CONSTRAINT pk_advertisement PRIMARY KEY (advertisementID),
+  CONSTRAINT fk_user_advertisement FOREIGN KEY (userID) REFERENCES Users(userID) ON DELETE CASCADE,
+  CONSTRAINT  fk_moderator_advertisement FOREIGN KEY (moderatorID) REFERENCES Moderators(UserID),
+  CONSTRAINT  fk_category_advertisement FOREIGN KEY (categoryID) REFERENCES Categories(categoryID) ON DELETE RESTRICT,
+  CONSTRAINT  fk_status_advertisement FOREIGN KEY (statusID) REFERENCES Statuses(statusID) ON DELETE RESTRICT
+);
