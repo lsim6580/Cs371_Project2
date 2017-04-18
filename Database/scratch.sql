da85cff2ec670371f36f1c8650117907199dc0eb
Jay Wolf <jwolf76dev@gmail.com>
2017-04-18 15:17:10 -0500

    minor code cleanup

--- Database/scratch.sql
+++ Database/scratch.sql
@@ -17,7 +17,8 @@ Create TABLE IF NOT EXISTS Users (
 CREATE TABLE IF NOT EXISTS Moderators (
   userID VARCHAR(20) NOT NULL,
   CONSTRAINT pk_moderators PRIMARY KEY (UserID),
-  CONSTRAINT fk_moderator_userID FOREIGN KEY (UserID) REFERENCES Users(UserID) ON DELETE RESTRICT
+  CONSTRAINT fk_moderator_userID FOREIGN KEY (UserID) REFERENCES Users(UserID)
+  ON DELETE RESTRICT
 );
 
 CREATE TABLE IF NOT EXISTS Statuses (
@@ -35,18 +36,22 @@ CREATE TABLE IF NOT EXISTS Categories (
 CREATE TABLE IF NOT EXISTS Advertisements (
   advertisementID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
   advertisementTitle VARCHAR(20) NOT NULL,
-  advertisementDetails TEXT NOT NULL ,
+  advertisementDetails TEXT NOT NULL,
   advertisementDateTime DATETIME NOT NULL,
   price FLOAT(10,2) NOT NULL,
+  categoryID VARCHAR(3) NOT NULL,
   userID VARCHAR(20) NOT NULL,
   moderatorID VARCHAR(20),
-  categoryID VARCHAR(3) NOT NULL,
   statusID VARCHAR(2) NOT NULL,
   CONSTRAINT pk_advertisement PRIMARY KEY (advertisementID),
-  CONSTRAINT fk_user_advertisement FOREIGN KEY (userID) REFERENCES Users(userID) ON DELETE CASCADE,
-  CONSTRAINT  fk_moderator_advertisement FOREIGN KEY (moderatorID) REFERENCES Moderators(UserID) ON DELETE SET NULL,
-  CONSTRAINT  fk_category_advertisement FOREIGN KEY (categoryID) REFERENCES Categories(categoryID) ON DELETE RESTRICT,
-  CONSTRAINT  fk_status_advertisement FOREIGN KEY (statusID) REFERENCES Statuses(statusID) ON DELETE RESTRICT
+  CONSTRAINT fk_category_advertisement FOREIGN KEY (categoryID) REFERENCES Categories(categoryID)
+  ON DELETE RESTRICT,
+  CONSTRAINT fk_user_advertisement FOREIGN KEY (userID) REFERENCES Users(userID)
+  ON DELETE CASCADE,
+  CONSTRAINT fk_moderator_advertisement FOREIGN KEY (moderatorID) REFERENCES Moderators(UserID)
+  ON DELETE SET NULL,
+  CONSTRAINT fk_status_advertisement FOREIGN KEY (statusID) REFERENCES Statuses(statusID)
+  ON DELETE RESTRICT
 );
 /* end create tables */
 
